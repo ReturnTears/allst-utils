7684f7a28579c14e02cc23f4962de649f83f7dea
returntears <returntears@outlook.com>
2019-05-21 21:41:04 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#allst utils
+##note 短信发送
+##push 消息推送(友盟)
+##websocket 数据推送
---
+++ src/main/java/allst/utils/AllstUtilsApplication.java
@@ -0,0 +1,13 @@
+package allst.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllstUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllstUtilsApplication.class, args);
+    }
+
+}
